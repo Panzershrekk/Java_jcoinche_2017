c0936411f245aaf168045fdf96e406884a2a011d
Thomas Fossaert <thomas.fossaert@epitech.eu>
2017-10-16 19:25:07 +0200

    [+] Initialisation du projet avec Maven

---
+++ Client/src/main/java/ClientMain.java
@@ -0,0 +1,9 @@
+import io.netty.channel.nio.NioEventLoop;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        String message = "Robert";
+        MessageUtil messageUtil = new MessageUtil(message);
+        messageUtil.printMessage();
+    }
+}
