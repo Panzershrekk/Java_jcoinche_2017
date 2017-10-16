ae1a68ffb6f4ad0df914508bb73222e178c044ea
Thomas Fossaert <thomas.fossaert@epitech.eu>
2017-10-16 20:08:10 +0200

    [+] Setup JUnit et Netty côté client

--- Client/src/main/java/ClientMain.java
+++ Client/src/main/java/ClientMain.java
@@ -1,6 +1,9 @@
+import io.netty.channel.nio.NioEventLoopGroup;
+
 public class ClientMain {
     public static void main(String[] args) {
         String message = "Robert";
+        NioEventLoopGroup event;
         MessageUtil messageUtil = new MessageUtil(message);
         messageUtil.printMessage();
     }
