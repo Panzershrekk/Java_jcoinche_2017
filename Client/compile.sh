ce34afdecb91b26ab58ce31076200c958de11de5
Thomas Fossaert <thomas.fossaert@epitech.eu>
2017-10-18 09:08:27 +0200

    Build avec dépendance

--- Client/compile.sh
+++ Client/compile.sh
@@ -1,2 +1,2 @@
 mvn clean install
-cp -rf ./target/jcoinche-client-1.0-SNAPSHOT.jar ../jcoinche-client.jar
+cp -rf ./target/jcoinche-client-1.0-SNAPSHOT-jar-with-dependencies.jar ../jcoinche-client.jar
--- Server/compile.sh
+++ Server/compile.sh
@@ -1,2 +1,2 @@
 mvn clean install
-cp -rf ./target/jcoinche-server-1.0-SNAPSHOT.jar ../jcoinche-server.jar
+cp -rf ./target/jcoinche-server-1.0-SNAPSHOT-jar-with-dependencies.jar ../jcoinche-server.jar
